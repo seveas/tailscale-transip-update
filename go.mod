f45c9e4f3ede0eb6cbd7b7a3536b4fefeef0aa14
Dennis Kaarsemaker <dennis@kaarsemaker.net>
2021-12-18 18:38:16 +0100

    Add sshfp recods for tailscale hosts

--- go.mod
+++ go.mod
@@ -5,19 +5,20 @@ go 1.17
 require (
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/sirupsen/logrus v1.8.1
-	github.com/spf13/cobra v1.2.1
-	github.com/spf13/viper v1.9.0
-	github.com/transip/gotransip/v6 v6.6.2
-	tailscale.com v1.16.1
+	github.com/spf13/cobra v1.3.0
+	github.com/spf13/viper v1.10.1
+	github.com/transip/gotransip/v6 v6.11.0
+	tailscale.com v1.18.2
 )
 
 require (
+	github.com/akutz/memconn v0.1.0 // indirect
 	github.com/fsnotify/fsnotify v1.5.1 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/magiconair/properties v1.8.5 // indirect
 	github.com/mitchellh/go-ps v1.0.0 // indirect
-	github.com/mitchellh/mapstructure v1.4.2 // indirect
+	github.com/mitchellh/mapstructure v1.4.3 // indirect
 	github.com/pelletier/go-toml v1.9.4 // indirect
 	github.com/spf13/afero v1.6.0 // indirect
 	github.com/spf13/cast v1.4.1 // indirect
@@ -25,13 +26,13 @@ require (
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.2.0 // indirect
 	github.com/transip/gotransip v5.8.2+incompatible // indirect
-	go4.org/intern v0.0.0-20210108033219-3eb7198706b2 // indirect
-	go4.org/mem v0.0.0-20201119185036-c04c5a6ff174 // indirect
-	go4.org/unsafe/assume-no-moving-gc v0.0.0-20201222180813-1025295fd063 // indirect
-	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
-	golang.org/x/sys v0.0.0-20210906170528-6f6e22806c34 // indirect
+	go4.org/intern v0.0.0-20211027215823-ae77deb06f29 // indirect
+	go4.org/mem v0.0.0-20210711025021-927187094b94 // indirect
+	go4.org/unsafe/assume-no-moving-gc v0.0.0-20211027215541-db492cf91b37 // indirect
+	golang.org/x/crypto v0.0.0-20211215153901-e495a2d5b3d3 // indirect
+	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
 	golang.org/x/text v0.3.7 // indirect
-	gopkg.in/ini.v1 v1.63.2 // indirect
+	gopkg.in/ini.v1 v1.66.2 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
-	inet.af/netaddr v0.0.0-20210721214506-ce7a8ad02cc1 // indirect
+	inet.af/netaddr v0.0.0-20211027220019-c74959edd3b6 // indirect
 )
--- main.go
+++ main.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"context"
+	"crypto/sha1"
+	"crypto/sha256"
+	"fmt"
+	"net"
 	"os"
 	"path/filepath"
 	"strings"
@@ -13,6 +17,7 @@ import (
 	"github.com/spf13/viper"
 	"github.com/transip/gotransip/v6"
 	tipdomain "github.com/transip/gotransip/v6/domain"
+	"golang.org/x/crypto/ssh"
 	"tailscale.com/client/tailscale"
 )
 
@@ -48,6 +53,7 @@ func init() {
 	rootCmd.Flags().StringP("subdomain", "s", "", "Which subdomain the records should be updated under")
 	rootCmd.Flags().DurationP("expire", "e", time.Hour, "Expiry for new records")
 	rootCmd.Flags().BoolP("verbose", "v", false, "Verbose output")
+	rootCmd.Flags().BoolP("sshfp", "f", false, "Also create or update sshfp records")
 
 	viper.BindPFlag("User", rootCmd.Flags().Lookup("user"))
 	viper.BindPFlag("Key", rootCmd.Flags().Lookup("key"))
@@ -55,6 +61,7 @@ func init() {
 	viper.BindPFlag("Subdomain", rootCmd.Flags().Lookup("subdomain"))
 	viper.BindPFlag("Expire", rootCmd.Flags().Lookup("expire"))
 	viper.BindPFlag("Verbose", rootCmd.Flags().Lookup("verbose"))
+	viper.BindPFlag("SSHFP", rootCmd.Flags().Lookup("sshfp"))
 }
 
 func main() {
@@ -65,9 +72,27 @@ func run(cmd *cobra.Command, args []string) {
 	user := viper.GetString("User")
 	key, _ := homedir.Expand(viper.GetString("Key"))
 	domain := strings.Trim(viper.GetString("Domain"), ".")
+	sshfp := viper.GetBool("SSHFP")
 	sub := strings.Trim(strings.TrimSuffix(strings.Trim(viper.GetString("SubDomain"), "."), domain), ".")
 	if sub != "" {
 		sub = "." + sub
+	}
+	var currentKey ssh.PublicKey
+	sshConfig := &ssh.ClientConfig{
+		Timeout: 1 * time.Second,
+		HostKeyCallback: func(hostname string, remote net.Addr, key ssh.PublicKey) error {
+			currentKey = key
+			return nil
+		},
+	}
+	keyTypes := map[string]int{
+		"ssh-rsa":             1,
+		"ecdsa-sha2-nistp256": 3,
+		"ssh-ed25519":         4,
+	}
+	hashTypes := map[int]func([]byte) []byte{
+		1: func(b []byte) []byte { r := sha1.Sum(b); return r[:] },
+		2: func(b []byte) []byte { r := sha256.Sum256(b); return r[:] },
 	}
 	expire := viper.GetDuration("Expire")
 
@@ -127,9 +152,45 @@ func run(cmd *cobra.Command, args []string) {
 				}
 			}
 		}
+		if sshfp {
+			for keyType := range keyTypes {
+				sshConfig.HostKeyAlgorithms = []string{keyType}
+				currentKey = nil
+				ssh.Dial("tcp", peer.TailscaleIPs[0].String()+":22", sshConfig)
+				if currentKey != nil {
+					data := currentKey.Marshal()
+					for hashType, hashFunc := range hashTypes {
+						typ := "SSHFP"
+						prefix := fmt.Sprintf("%d %d ", keyTypes[keyType], hashType)
+						record := fmt.Sprintf("%s%x", prefix, hashFunc(data))
+						found := false
+						for _, entry := range entries {
+							if entry.Name == name && entry.Type == typ && strings.HasPrefix(entry.Content, prefix) {
+								found = true
+								if entry.Content == record {
+									log.Debugf("%s record for %s.%s has not changed", typ, name, domain)
+								} else {
+									log.Infof("updating %s record for %s.%s from %s to %s", typ, name, domain, entry.Content, record)
+									entry.Content = record
+									err := repo.UpdateDNSEntry(domain, entry)
+									if err != nil {
+										bail("Unable to update %s record for %s: %s", typ, name, err)
+									}
+								}
+							}
+						}
+						if !found {
+							log.Infof("adding %s record for %s.%s pointing to %s", typ, name, domain, record)
+							err := repo.AddDNSEntry(domain, tipdomain.DNSEntry{Name: name, Expire: int(expire / time.Second), Type: typ, Content: record})
+							if err != nil {
+								bail("Unable to add %s record for %s: %s", typ, name, err)
+							}
+						}
+					}
+				}
+			}
+		}
 	}
-	_ = client
-
 }
 
 func bail(format string, args ...interface{}) {
